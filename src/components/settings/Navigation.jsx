78339a8414dc34c543a34c51970f7add41d3ecc6
filipKovachev <kovachev@progress.com>
2022-11-20 23:21:18 +0200

    change nacigation button color

--- src/components/settings/Navigation.jsx
+++ src/components/settings/Navigation.jsx
@@ -5,15 +5,15 @@ export const Navigation = () => {
    return <div className="navigation-container">
       <div class="k-tilelayout-item-header k-card-header k-cursor-grab">
     <Link to="/home/account" className="dashboard-button" style={{ textDecoration: 'none' }}>
-  <button className="k-button k-button-md k-rounded-md k-button-flat k-button-flat-base" type="button"><span className="k-icon k-i-user k-button-icon"></span><span className="k-button-text">Account</span></button>
+  <button style={{color: 'black'}} className="k-button k-button-md k-rounded-md k-button-flat k-button-flat-base" type="button"><span className="k-icon k-i-user k-button-icon"></span><span className="k-button-text">Account</span></button>
   </Link>
 
   <Link to="/home/billing" className="dashboard-button" style={{ textDecoration: 'none' }}>
-  <button className="k-button k-button-md k-rounded-md k-button-flat k-button-flat-base" type="button"><span className="k-icon k-i-file-txt k-button-icon"></span><span className="k-button-text">Billing</span></button>
+  <button style={{color: 'black'}} className="k-button k-button-md k-rounded-md k-button-flat k-button-flat-base" type="button"><span className="k-icon k-i-file-txt k-button-icon"></span><span className="k-button-text">Billing</span></button>
   </Link>
 
   <Link to="/home/notifications" className="dashboard-button" style={{ textDecoration: 'none' }}>
-  <button className="k-button k-button-md k-rounded-md k-button-flat k-button-flat-base" type="button" ><span className="k-icon k-i-notification k-button-icon"></span><span className="k-button-text">Notifications</span></button>
+  <button style={{color: 'black'}} className="k-button k-button-md k-rounded-md k-button-flat k-button-flat-base" type="button" ><span className="k-icon k-i-notification k-button-icon"></span><span className="k-button-text">Notifications</span></button>
   </Link>
 </div>
    </div>

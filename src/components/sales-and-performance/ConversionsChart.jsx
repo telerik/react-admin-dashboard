9ff5ee3bcae2c211bb64562e63ff1f0129abef46
filipKovachev <kovachev@progress.com>
2022-11-30 17:47:10 +0200

    remove redundant code, change color

--- src/components/sales-and-performance/ConversionsChart.jsx
+++ src/components/sales-and-performance/ConversionsChart.jsx
@@ -1,49 +1,33 @@
 import * as React from 'react';
 import { CircularGauge } from '@progress/kendo-react-gauges';
 
-const colors = [
-  {
-    color: '#FF6358',
-  },
-];
 export const ConversionsChart = () => {
+
   const [value] = React.useState(78);
   const [secondValue] = React.useState(20);
 
   const arcOptions = {
     value: value,
-    colors,
     transitions: false,
   };
-
 
   const arcSecondOptions = {
     value: secondValue,
-    colors,
     transitions: false,
   };
   
-  const arcCenterRenderer = (color) => {
+  const arcCenterRenderer = () => {
     return (
-      <h3
-        style={{
-          color: color,
-        }}
-      >
+      <h3>
         +3.1K
-
       </h3>
       
     );
   };
 
-  const secondArcCenterRenderer = (color) => {
+  const secondArcCenterRenderer = () => {
     return (
-      <h3
-        style={{
-          color: '#666666'
-        }}
-      >
+      <h3>
         -1.2K
       </h3>
     );
@@ -80,8 +64,8 @@ export const ConversionsChart = () => {
         </div>
        
        <div>
-       <CircularGauge {...arcSecondOptions} arcCenterRender={secondArcCenterRenderer}  style={{height: '120px'}}
-  scale={{
+       <CircularGauge {...arcSecondOptions} arcCenterRender={secondArcCenterRenderer}  style={{height: '120px'}} color='#666666'
+     scale={{
           startAngle: 300,
         }}/>
          <div
